86cb4353e35eb0f596c692c6b5c60aedac6b2fef
임시우 (Siwoo Lim) <siwoo5530@gmail.com>
2022-10-28 13:09:42 +0900

    init project

---
+++ main.py
@@ -0,0 +1,71 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+
+# 웹캠, 영상 파일의 경우 이것을 사용하세요.:
+cap = cv2.VideoCapture(0)
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    c = np.array(c)  # End
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+if __name__ == "__main__":
+    with mp_pose.Pose(
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("카메라를 찾을 수 없습니다.")
+                # 동영상을 불러올 경우는 'continue' 대신 'break'를 사용합니다.
+                continue
+
+            # 필요에 따라 성능 향상을 위해 이미지 작성을 불가능함으로 기본 설정합니다.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = pose.process(image)
+
+            # 포즈 주석을 이미지 위에 그립니다.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+            # 보기 편하게 이미지를 좌우 반전합니다.
+            cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+            # print(results.pose_landmarks)
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+                # print(landmarks)
+            except:
+                pass
+
+
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            print(calculate_angle(shoulder, elbow, wrist))
+
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    cap.release()
+
+
+
